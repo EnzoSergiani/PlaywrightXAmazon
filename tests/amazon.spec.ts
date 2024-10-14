15af76b0be8163b1e17e76fd19f1847f5bb90ed6
AtnRic <antoine.richard@student.junia.com>
2024-10-14 16:55:02 +0200

    recherche dans catégorie + ajout panier
    ok pour  chrome & firefox

--- tests/amazon.spec.ts
+++ tests/amazon.spec.ts
@@ -15,7 +15,7 @@ test.describe("Amazon Feature Testing", () => {
 
 test.describe("Amazon Login Testing", () => {
   test("access to the login page", async ({ page }) => {
-    await page.goto("https://www.amazon.com");
+    await page.goto("https://www.amazon.fr");
     await page.click("#nav-link-accountList"); //! fonctione sur firefox mais pas sur chrome
     await page.fill("#ap_email", "your-email@example.com");
     await page.click("#continue");
