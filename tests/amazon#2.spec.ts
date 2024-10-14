9f0cddee8e1933265b7b58a729c8449d52887fd9
AdamVidal <adam.vidal@student.junia.com>
2024-10-14 16:15:53 +0200

    refuser cookies, supprimer panier

--- tests/amazon#2.spec.ts
+++ tests/amazon#2.spec.ts
@@ -3,9 +3,11 @@ import { test, expect } from '@playwright/test';
 test.describe("Amazon put item in cart test", () => {
   test("open cart", async ({ page }) => {
     await page.goto("https://www.amazon.fr"); // on va sur amazon.fr
+    await page.click("#sp-cc-rejectall-link"); // on refuse les cookies
     await page.fill("#twotabsearchtextbox", "souris"); // on met souris dans la barre de recherhe
     await page.click("#nav-search-submit-button"); // on clique sur le bouton de recherche
     await page.click("#a-autoid-7-announce"); // on ajoute le premier article au panier
-    await page.click("#nav_cart"); // on clique sur le panier
+    await page.click("#nav-cart"); // on clique sur le panier
+    await page.click(".sc-action-delete"); // on supprime l'article du panier
   })
 });
