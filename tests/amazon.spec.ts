23f33eac91f01f7753e4214e2cd3e98ab3880863
ThibautLemichel <thibaut.lemichel@student.junia.com>
2024-10-14 17:04:49 +0200

    variable +log in + register

--- tests/amazon.spec.ts
+++ tests/amazon.spec.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const { test, expect } = require("@playwright/test");
 
 // test("<description_du_test>", async ({ page }) => {
@@ -24,43 +23,3 @@ test.describe("Amazon Login Testing", () => {
     await page.click("#signInSubmit");
   });
 });
-
-test.describe("Amazon Search Testing", () => {
-  test("access to the search page", async ({ page }) => {
-    await page.goto("https://www.amazon.fr");
-    await page.fill("#twotabsearchtextbox", "mac mini m1"); //remplir le champ de recherche
-    await page.click("#sp-cc-accept"); //cliquer sur accepter les cookies
-    await page.click("#nav-search-submit-button"); //cliquer sur le bouton de recherche
-  });
-});
-
-test.describe("Amazon Cart Testing", () => {
-  test("access to the cart page", async ({ page }) => {
-    await page.goto("https://www.amazon.fr");
-    await page.fill("#twotabsearchtextbox", "mac mini m1"); //remplir le champ de recherche
-    await page.click("#sp-cc-accept"); //cliquer sur accepter les cookies
-    await page.click("#nav-search-submit-button"); //cliquer sur le bouton de recherche
-    const firstProduct = await page
-    .locator(".s-main-slot .s-result-item")
-    .first();
-    await firstProduct.click();   //cliquer sur le premier produit
-    await page.click("#add-to-cart-button"); //ajouter au panier
-  });
-});
-
-test.describe("Amazon Payment Testing", () => {
-  test("access to the payment page", async ({ page }) => {
-    await page.goto("https://www.amazon.fr");
-    await page.fill("#twotabsearchtextbox", "mac mini m1"); //remplir le champ de recherche
-    await page.click("#sp-cc-accept"); //cliquer sur accepter les cookies
-    await page.click("#nav-search-submit-button"); //cliquer sur le bouton de recherche
-    const firstProduct = await page
-    .locator(".s-main-slot .s-result-item")
-    .first();
-    await firstProduct.click();   //cliquer sur le premier produit
-    await page.click("#add-to-cart-button"); //ajouter au panier
-    await page.click("#sc-buy-box-ptc-button"); //passer à la caisse
-  });
-});
-=======
->>>>>>> main
