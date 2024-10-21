ea75224ed1a4c6c7f4a2766017d58e92b15f3e14
AdamVidal <adam.vidal@student.junia.com>
2024-10-21 14:44:39 +0200

    expect panier vide

--- tests/amazon#2.spec.ts
+++ tests/amazon#2.spec.ts
@@ -4,7 +4,7 @@ test.describe("Rechercher un produit, le mettre dans le panier, voir le panier p
   test("Rechercher un produit, le mettre dans le panier, voir le panier puis le supprimer", async ({ page }) => {
     await page.goto("https://www.amazon.fr"); // on va sur amazon.fr
     await page.click("#sp-cc-rejectall-link"); // on refuse les cookies
-    await page.fill("#twotabsearchtextbox", "souris"); // on met souris dans la barre de recherhe
+    await page.fill("#twotabsearchtextbox", "laptop"); // on fait une recherche
     await page.click("#nav-search-submit-button"); // on clique sur le bouton de recherche
     const firstProduct = await page
       .locator(".s-main-slot .s-result-item")
@@ -13,5 +13,7 @@ test.describe("Rechercher un produit, le mettre dans le panier, voir le panier p
     await page.click("#add-to-cart-button"); // ajouter le produit au panier
     await page.click("#nav-cart"); // on clique sur le panier
     await page.click('input[data-action="delete"]'); // on supprime l'article du panier
+    const emptyCartMessage = await page.locator('text=Votre panier Amazon est vide');
+    await expect(emptyCartMessage).toBeVisible();
   })
 });
